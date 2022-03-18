460d09ac137b0aa77160dc80f348c4ad38d0ce70
Vaibhav mama <vaibhav.jamadagni.gmail.com>
2022-03-18 12:35:55 +0530

    ride match route

--- producer/index.js
+++ producer/index.js
@@ -8,5 +8,9 @@ app.post("/new_ride", (req, res) => {
   console.log(req.body);
   res.send(req.body);
 });
+app.post("/new_ride_matching_consumer", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
