131b28d7d8ea9a07187dd30a1dc322556b99108a
nitish velu <nitishvelu@gmail.com>
2022-03-28 10:28:08 +0530

    db schema update

--- database/db.js
+++ database/db.js
@@ -2,23 +2,21 @@ import express from 'express'
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 const app = express()
-app.get('/feed', async (req, res) => {
-  const posts = await prisma.post.findMany({
-    where: { published: true },
-    include: { author: true },
-  })
-  res.json(posts)
+app.get('/ride', async (req, res) => {
+  const rides = await prisma.ride.findMany()
+  res.json(rides)
 })
-app.post('/post', async (req, res) => {
-  const { title, content, authorEmail } = req.body
-  const post = await prisma.post.create({
+app.post('/ride', async (req, res) => {
+  const { cost, pickup, destination,seats,time } = req.body
+  const ride = await prisma.ride.create({
     data: {
-      title,
-      content,
-      published: false,
-      author: { connect: { email: authorEmail } },
+        cost,
+        pickup,
+        destination,
+        seats,
+        time,
     },
   })
-  res.json(post)
+  res.json(ride)
 })
 app.listen(4000, () => console.log('Server started on port 4000'))
