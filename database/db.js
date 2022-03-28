292390dd5a9f03f8174758ea53a20e65667d4680
nitish velu <nitishvelu@gmail.com>
2022-03-28 15:22:33 +0530

    working database

--- database/db.js
+++ database/db.js
@@ -1,20 +1,25 @@
-import express from 'express'
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+const express = require("express");
+const { PrismaClient } = require('@prisma/client');
+
 const app = express()
+
+app.use(express.json());
+
+const prisma = new PrismaClient()
 app.get('/ride', async (req, res) => {
   const rides = await prisma.ride.findMany()
   res.json(rides)
 })
 app.post('/ride', async (req, res) => {
-  const { cost, pickup, destination,seats,time } = req.body
+  const { cost, pickup, destination,seats,time,name} = req.body
   const ride = await prisma.ride.create({
     data: {
-        cost,
-        pickup,
-        destination,
-        seats,
-        time,
+        cost: Number(cost),
+        pickup:pickup,
+        destination:destination,
+        seats:Number(seats),
+        time:Number(time),
+        name,
     },
   })
   res.json(ride)
