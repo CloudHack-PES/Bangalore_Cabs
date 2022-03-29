c851650d90677fc6ecb90b6af336c261a2180ff6
nitish velu <nitishvelu@gmail.com>
2022-03-29 22:04:33 +0530

    next js progress bar

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,13 +1,16 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { Flex } from "@chakra-ui/react";
 import Nav from "../components/Nav";
+import NextNProgress from "nextjs-progressbar";
+
 
 
 function MyApp({ Component, pageProps }) {
   return(<ChakraProvider>
     <Flex flexDir="column">
-      <Nav />
-      <Component {...pageProps}/>
+    <NextNProgress color="#68D391" height={5} />
+    <Nav />
+    <Component {...pageProps}/>
     </Flex>
     </ChakraProvider> )
 }
