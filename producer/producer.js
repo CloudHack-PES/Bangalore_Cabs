b7435a2e663ea680a1d42ea72a32f5ab70a65fc1
nitish velu <nitishvelu@gmail.com>
2022-03-29 21:30:37 +0530

    cors

--- producer/producer.js
+++ producer/producer.js
@@ -3,6 +3,15 @@ const express = require("express");
 const app = express();
 const amqp = require("amqplib");
 
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
+
 app.use(express.json());
 
 connect();
